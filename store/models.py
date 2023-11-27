51c5a445db1d563a6195f74bac16a8bebe02607e
Lucas Acosta <lucas.heck@live.com>
2023-11-27 17:18:49 -0300

    inital commit

---
+++ store/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Cliente(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True)
